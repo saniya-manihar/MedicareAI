993f7a06520a262f1175ef05d4f5bbea168d32e6
Your Name <saniyasiddiqui56505@gmail.com>
2026-07-10 00:44:00 +0530

    Fix requirements

---
+++ rag/vector_db.py
@@ -0,0 +1,26 @@
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+
+loader = PyMuPDFLoader("data/medical_rag_test.pdf")
+documents = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=50
+)
+
+chunks = splitter.split_documents(documents)
+
+embedding = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+vector_db = Chroma.from_documents(
+    documents=chunks,
+    embedding=embedding,
+    persist_directory="chroma_db"
+)
+
+print("✅ Vector database created successfully!")
